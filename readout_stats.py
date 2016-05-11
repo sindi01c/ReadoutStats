313923478830648e87ba68ed849c1b0de104f2a2
Catalin Sindilaru <sindi01c@flightdataservices.com>
2016-05-11 11:35:20 +0100

    Swithhing to Generation of individual points rather than whole averages

--- readout_stats.py
+++ readout_stats.py
@@ -37,7 +37,9 @@ from analysis_engine.utils import open_node_container
 
 
 
-averages = defaultdict(lambda: defaultdict(list))
+#averages = defaultdict(lambda: defaultdict(list))
+
+flights = defaultdict(lambda: defaultdict(dict))
 
 
 for filename in os.listdir('.'):
@@ -62,25 +64,37 @@ for filename in os.listdir('.'):
                 pass
         
         for parameter in parameters.itervalues():
-            
+        
             for phase in phases.itervalues():
                 arrays = []
                 for section in phase:
                     arrays.append(parameter.array[section.slice.start * parameter.hz:section.slice.stop * parameter.hz])
                 array = np.ma.concatenate(arrays)
                 
-                averages[parameter.name][phase.name].append(np.ma.mean(array))
+                flights[flight_pk][parameter.name][phase.name] = np.ma.mean(array)
+    break
+                #averages[parameter.name][phase.name].append(np.ma.mean(array))
                 #writer.writerow((parameter.name, phase.name, np.ma.mean(array)))
                 
                 #for raw_value, state_name in parameter.array.values_mapping.iteritems():
                     #state_averages[parameter.name][phase.name][state_name].append(np.ma.mean(array))
     
 
-with open('output.csv', 'wb') as file_obj:
-    writer = csv.writer(file_obj)
-    for parameter, phases in averages.iteritems():
-        for phase, values in phases.iteritems():
-            writer.writerow((parameter, phase, np.ma.mean(values)))
+def write_csv():
+    with open('output.csv', 'wb') as file_obj:
+        writer = csv.writer(file_obj)
+        for flight_pk, parameter_phases in flights.iteritems():
+            for parameter, phases in parameter_phases.iteritems():
+                for phase, value in phases.iteritems():
+                    writer.writerow((flight_pk, parameter, phase, value, attrs))
+
+while True:
+    try:
+        write_csv()
+    except:
+        pass
+    else:
+        break
 
 
         #speedbrake = parameters.get('Speedbrake')
