f7a5fec64985e5e46bc0164826437c2be750156b
Catalin Sindilaru <sindi01c@flightdataservices.com>
2016-05-18 11:21:29 +0100

    Revert ".csv files and pdf generation"
    This reverts commit 35291400578726db3043af109053bde45807d775.

--- readout_stats.py
+++ readout_stats.py
@@ -41,7 +41,8 @@ from analysis_engine.utils import open_node_container
 #averages = defaultdict(lambda: defaultdict(list))
 flights = defaultdict(lambda: defaultdict(dict))
 
-for filename in os.listdir('.'):
+
+for filename in os.listdir('D:\\ReadoutStats\\flight_data\\.'):
     match = re.match(r'^(?P<frame_type>.*)\.zip$', filename)
     if not match:
         continue
@@ -74,7 +75,7 @@ for filename in os.listdir('.'):
                     continue
                 
                 flights[flight_pk][parameter.name][phase.name] = np.ma.mean(array)
-    #break
+    break
                 #averages[parameter.name][phase.name].append(np.ma.mean(array))
                 #writer.writerow((parameter.name, phase.name, np.ma.mean(array)))
                 
@@ -83,13 +84,13 @@ for filename in os.listdir('.'):
     
 
 def write_csv():
-    with open('D:\\ReadoutStats\\csv_flight_data\\A380.csv', 'wb') as file_obj:
+    with open('D:\\ReadoutStats\\csv_flight_data\\output.csv', 'wb') as file_obj:
         writer = csv.writer(file_obj)
-        writer.writerow(('flight_pk', 'parameter', 'phases', 'value'))
+        writer.writerow(('flight_pk', 'parameter', 'phases', 'value', 'attrs'))
         for flight_pk, parameter_phases in flights.iteritems():
             for parameter, phases in parameter_phases.iteritems():
                 for phase, value in phases.iteritems():
-                    writer.writerow((flight_pk, parameter, phase, value))
+                    writer.writerow((flight_pk, parameter, phase, value, attrs))
 
 while True:
     try:
@@ -98,3 +99,16 @@ while True:
         pass
     else:
         break
+
+
+        #speedbrake = parameters.get('Speedbrake')
+        #if not speedbrake:
+            #continue
+        #airborne = phases.get('Descending')
+        #if not airborne:
+            #continue
+        #arrays = []
+        #for air in airborne:
+            #arrays.append(speedbrake.array[air.slice])
+        #print np.ma.concatenate(arrays)
+
