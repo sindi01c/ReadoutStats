bcdbc3ac58c590c4a51d628c275c4e05b04cec01
Catalin Sindilaru <sindi01c@flightdataservices.com>
2016-06-02 09:20:45 +0100

    skip on errors

--- readout_stats.py
+++ readout_stats.py
@@ -6,6 +6,7 @@ import statsmodels.robust as rb
 import pandas as pd
 import itertools
 import seaborn as sns
+import zipfile
 
 
 from collections import defaultdict
@@ -50,46 +51,51 @@ medians = defaultdict(lambda: defaultdict(list))
 
 count = 0
 for filename in os.listdir('.'):
+
     match = re.match(r'^(?P<frame_type>.*)\.zip$', filename)
     if not match:
         continue
     frame_name = match.groups()[0]
     print frame_name
     
-    for flight_pk, nodes, attrs in open_node_container(filename):
-        count += 1
-        print flight_pk, attrs, count
-        
-        phases = {}
-        parameters = {}
-        for node_name, node in nodes.iteritems():
-            if isinstance(node, SectionNode):
-                phases[node_name] = node
-            elif type(node) == DerivedParameterNode and node.array.dtype.kind !='S':
-                parameters[node_name] = node
-            else:
-                # TODO: MultistateDerivedParameterNode
-                pass
-        
-        for parameter in parameters.itervalues():
-            #if parameter in DERIVED_EXCLUSIONS[frame_name]:
-                #continue
-            for phase in phases.itervalues():
-                arrays = []
-                for section in phase:
-                    arrays.append(parameter.array[section.slice.start * parameter.hz:section.slice.stop * parameter.hz])
-                array = np.ma.concatenate(arrays)
-                
-                median = np.ma.median(array)
-                if np.ma.count(median):
-                    medians[parameter.name][phase.name].append(float(median))
-                #medianabsolutedev[parameter.name][phase.name] = statsmodels.robust.scale.mad(array)
-                #writer.writerow((parameter.name, phase.name, np.ma.mean(array)))
-                                
-                #for raw_value, state_name in parameter.array.values_mapping.iteritems():
-                    #state_averages[parameter.name][phase.name][state_name].append(np.ma.mean(array))
-        #if count >= 10:
-            #break
+    try:
+        for flight_pk, nodes, attrs in open_node_container(filename):
+            count += 1
+            print flight_pk, attrs, count
+            
+            phases = {}
+            parameters = {}
+            for node_name, node in nodes.iteritems():
+                if isinstance(node, SectionNode):
+                    phases[node_name] = node
+                elif type(node) == DerivedParameterNode and node.array.dtype.kind !='S':
+                    parameters[node_name] = node
+                else:
+                    # TODO: MultistateDerivedParameterNode
+                    pass
+            
+            for parameter in parameters.itervalues():
+                #if parameter in DERIVED_EXCLUSIONS[frame_name]:
+                    #continue
+                for phase in phases.itervalues():
+                    arrays = []
+                    for section in phase:
+                        arrays.append(parameter.array[section.slice.start * parameter.hz:section.slice.stop * parameter.hz])
+                    array = np.ma.concatenate(arrays)
+                    
+                    median = np.ma.median(array)
+                    if np.ma.count(median):
+                        medians[parameter.name][phase.name].append(float(median))
+                    #medianabsolutedev[parameter.name][phase.name] = statsmodels.robust.scale.mad(array)
+                    #writer.writerow((parameter.name, phase.name, np.ma.mean(array)))
+                                    
+                    #for raw_value, state_name in parameter.array.values_mapping.iteritems():
+                        #state_averages[parameter.name][phase.name][state_name].append(np.ma.mean(array))
+            #if count >= 10:
+                #break
+    except (RuntimeError, TypeError, NameError, zipfile.BadZipfile):
+        pass
+            
 
 def IQR(values):
     q75, q25 = np.percentile(values, [75, 25])
@@ -103,7 +109,7 @@ def IQR(values):
 
    
 
-with open('D:\\ReadoutStats\\1. fleet__csv_flight_data\\A319-320-321.csv', 'wb') as file_obj:
+with open('D:\\ReadoutStats\\all_fleets.csv', 'wb') as file_obj:
     writer = csv.writer(file_obj)
     writer.writerow(('parameter', 'phases', 'Average', 'Median', 'StdDev', 'MAD', 'q25','q75', 'lt','ut', 'min', 'max'))
     for parameter, phases in sorted(medians.items()):
