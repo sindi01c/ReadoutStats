3c551597919de742ca4ba802a9139a3e5ccf4a98
Catalin Sindilaru <sindi01c@flightdataservices.com>
2016-06-10 08:44:00 +0100

    filename

--- readout_stats.py
+++ readout_stats.py
@@ -89,7 +89,7 @@ for filename in os.listdir('.'):
     
 
 def write_csv():
-    with open('D:\\ReadoutStats\\ms_individual_1.csv', 'wb') as file_obj:
+    with open('D:\\ReadoutStats\\ms_individual_2.csv', 'wb') as file_obj:
         writer = csv.writer(file_obj)
         writer.writerow(('flight_pk', 'parameter', 'phases', 'states', 'value'))
         for flight_pk, parameter_phases in flights.iteritems():
